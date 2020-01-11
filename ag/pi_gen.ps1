b0ee21d05122f0e6dd66d976763818343477ca51
mitmih <38176228+mitmih@users.noreply.github.com>
2020-01-12 01:15:48 +0300

    1

--- ag/pi_gen.ps1
+++ ag/pi_gen.ps1
@@ -1,7 +1,7 @@
 [cmdletbinding()]
 param(
-    [alias('1')][Parameter(position=0)][uint16] $lim_min = 1,  # точность, кол-во знаков после запятой
-    [alias('2')][Parameter(position=1)][uint16] $lim_max = 6  # точность, кол-во знаков после запятой
+    [alias('1')][Parameter(position=0)][uint16] $lim_min = 0,  # точность, кол-во знаков после запятой
+    [alias('2')][Parameter(position=1)][uint16] $lim_max = 4  # точность, кол-во знаков после запятой
 )
 
 
@@ -9,16 +9,18 @@ Clear-Host
 
 # $lim_max = 5  # точность, кол-во знаков после запятой
 
-$x = @{0=3}  # числители для заданной точности
+$x = [ordered]@{}  # числители для заданной точности
 
-$y = @{0=1}  # знаменатели для заданной точности
+$y = [ordered]@{}  # знаменатели для заданной точности
 
 $ResultsTable = @()  # таблица с дробями для точностей из диапазона $lim_min..$lim_max
 
 # поиск числителя и знаменателя
-for ($digits = $lim_min; $digits -le $lim_max; $digits++)
+for ($digits = 0; $digits -le $lim_max; $digits++)
 {
-    for ($i = $y[($digits - 1)]; $i -lt 200000; $i++)
+    # for ($i = $y[($digits - 1)]; $i -lt $b; $i++)
+    $i = $y[($digits - 1)]
+    do
     {
         $a = [System.Math]::Round($i * [math]::pi, 0, 1) - 1
         
@@ -26,11 +28,11 @@ for ($digits = $lim_min; $digits -le $lim_max; $digits++)
         
         for ($j = $a; $j -le $b; $j++)   # между xPI - 1 и xPI + 1
         {
-            $err = [System.Math]::Round( ($j/$i - [math]::pi), $digits, 1)
+                $err = [System.Math]::Round( ($j/$i - [math]::pi), $digits, 1)
             
             if ($err -eq 0)
             {
-                $x[$digits] = $j
+                $x[[string]$digits] = $j
                 
                 break
             }
@@ -38,34 +40,39 @@ for ($digits = $lim_min; $digits -le $lim_max; $digits++)
         
         if ($err -eq 0)
         {
-            $y[$digits] = $i
+            $y[[string]$digits] = $i
             
             break
         }
         
-        if ($i % 1000 -eq 0 ) { @{$digits = ($j, $i)} }
+        # if ($i % 1000 -eq 0 ) { @{$digits = ($j, $i)} }
         
-    }
+        $i++
+    }  while ($true)
 }
 
+
 # формирование таблицы
-for ($i = $lim_min; $i -le $lim_max; $i++)
+for ($i = $lim_max; $i -ge $lim_min; $i--)
 {
     $ResultsTable += New-Object psobject -Property @{
         'TO4HOCTb'  = '{0,4}' -f $i
-        'DPOBb    ' = "{0,3} / {1,-3}" -f $x[$i], $y[$i]
+        'DPOBb    ' = "{0,3} / {1,-3}" -f $x["$i"], $y["$i"]
         '         ' = '{0, 4}' -f '='
-        '    ~    ' = "{0,-$($lim_max + 3):n$($i + 1)}" -f ([System.Math]::Round($x[$i] / $y[$i], $i + 1, 1))
-        '    PI   ' = "{0,-$($lim_max + 3)}" -f ([System.Math]::Round([math]::pi, $i + 1, 1))
+        '    ~    ' = "{0,-$($lim_max + 3):n$($i + 1)}" -f ([System.Math]::Round($x["$i"] / $y["$i"], $i + 0, 1))
+        '    PI   ' = "{0,-$($lim_max + 3):n$($i + 1)}" -f ([System.Math]::Round([math]::pi, $i + 1, 1))
     }
 }
 
+
 # вывод таблицы на экран
-$ResultsTable.GetEnumerator() <# | Sort-Object -Property 'TO4HOCTb' #> | Select-Object -Property `
+
+[math]::pi  # 3,14159265358979
+
+$ResultsTable.GetEnumerator() | Sort-Object -Property 'TO4HOCTb' | Select-Object -Property `
     'TO4HOCTb', `
     'DPOBb    ', `
     '         ', `
     '    ~    ', `
     '    PI   ' | Format-Table -Property *
 
-[math]::pi  # 3,14159265358979

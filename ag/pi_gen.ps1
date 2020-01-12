403f8e844e963a8f4683c30f4c897c6c6a091c91
mitmih <38176228+mitmih@users.noreply.github.com>
2020-01-12 18:26:08 +0300

    +

--- ag/pi_gen.ps1
+++ ag/pi_gen.ps1
@@ -1,35 +1,30 @@
 [cmdletbinding()]
 param(
     [alias('1')][Parameter(position=0)][uint16] $lim_min = 0,  # точность, кол-во знаков после запятой
-    [alias('2')][Parameter(position=1)][uint16] $lim_max = 3  # точность, кол-во знаков после запятой
+    [alias('2')][Parameter(position=1)][uint16] $lim_max = 2  # точность, кол-во знаков после запятой
 )
 
 
-function qwe {
-    param (
-        [string]    $str,
-        [int]       $t
-    )
-    
-    return ( (([string]( [math]::pi ))[0..($t + 1)] -join '') -eq $str )
-}
+$WatchDogTimer = [system.diagnostics.stopwatch]::startNew()  # профилирование
 
 Clear-Host
 
-# $lim_max = 5  # точность, кол-во знаков после запятой
 
 $x = [ordered]@{'0' = 3}  # числители для заданной точности
 
 $y = [ordered]@{'0' = 1}  # знаменатели для заданной точности
 
+$t = [ordered]@{'0' = $WatchDogTimer.Elapsed.TotalSeconds}  # отсечка таймера
+
 $ResultsTable = @()  # таблица с дробями для точностей из диапазона $lim_min..$lim_max
+
 
 # поиск числителя и знаменателя
 for ($digits = 1; $digits -le $lim_max; $digits++)
 {
-    # for ($i = $y[($digits - 1)]; $i -lt $b; $i++)
     $i = $y[($digits - 1)]
     do
+    # for ($i = $y[($digits - 1)]; $i -lt $b; $i++)
     {
         $a = [System.Math]::Floor($i * [math]::pi)      # вниз  до целого
         
@@ -59,13 +54,15 @@ for ($digits = 1; $digits -le $lim_max; $digits++)
         {
             $y[[string]$digits] = $i
             
+            $t[[string]$digits] = $WatchDogTimer.Elapsed.TotalSeconds
+            
             break
         }
         
         # if ($i % 1000 -eq 0 ) { @{$digits = ($j, $i)} }
         
         $i++
-    }  while ($true)
+    } while ($true)
 }
 
 
@@ -74,20 +71,22 @@ for ($digits = 1; $digits -le $lim_max; $digits++)
 for ($i = $lim_max; $i -ge $lim_min; $i--)
 {
     $ResultsTable += New-Object psobject -Property @{
-        'TO4HOCTb'  = "{0,$w}" -f $i
-        '  DPOBb  ' = "{0,$w} / {1,-$w}" -f $x["$i"], $y["$i"]
+        'TO4HOCTb'          = "{0,$w}" -f $i
+        '   4uc/\uTE/\b  '  = "{0,16}" -f $x["$i"]
+        '/'                 = '/'
+        '  3HAMEHATE/\b  '  = "{0,-16}" -f $y["$i"]
         '    PI   ' = "{0}" -f (( [string]( $x["$i"] / $y["$i"]) )[0..($i + 1)] -join '')
+        '      time      ' = "{0,7:n1}  {1,7}" -f $t["$i"], 'seconds'
+        # Write-Host ("{0,5:N1} minutes {1}" -f $WatchDogTimer.Elapsed.TotalMinutes, 'installation process launched') #_#
     }
     
     $ResultsTable += New-Object psobject -Property @{
         'TO4HOCTb'  = "{0,$w} " -f $i
-        '  DPOBb  ' = ''
         '    PI   ' = "{0}" -f (([string]( [math]::pi ))[0..($i + 1)] -join '')
     }
     
     $ResultsTable += New-Object psobject -Property @{
         'TO4HOCTb'  = "{0,$w}  " -f $i
-        '  DPOBb  ' = ''
         '    PI   ' = ''
     }
 }
@@ -98,6 +97,59 @@ for ($i = $lim_max; $i -ge $lim_min; $i--)
 [math]::pi  # 3,14159265358979
 
 $ResultsTable.GetEnumerator() | Sort-Object -Property 'TO4HOCTb' | Select-Object -Property `
-    'TO4HOCTb', `
-    '  DPOBb  ', `
-    '    PI   ' | Format-Table -Property *
+    'TO4HOCTb'          , `
+    '   4uc/\uTE/\b  '  , `
+    '/'                 , `
+    '  3HAMEHATE/\b  '  , `
+    '    PI   '         , `
+    '      time      '  | Format-Table -Property *
+<#
+3,14159265358979
+
+TO4HOCTb        4uc/\uTE/\b   /   3HAMEHATE/\b       PI
+--------     ---------------- - ---------------- ---------
+         0                  3 / 1                3
+         0                                       3.
+         0
+         1                 19 / 6                3.1
+         1                                       3.1
+         1
+         2                 22 / 7                3.14
+         2                                       3.14
+         2
+         3                245 / 78               3.141
+         3                                       3.141
+         3
+         4                333 / 106              3.1415
+         4                                       3.1415
+         4
+         5                355 / 113              3.14159
+         5                                       3.14159
+         5
+         6                355 / 113              3.141592
+         6                                       3.141592
+         6
+         7              86953 / 27678            3.1415926
+         7                                       3.1415926
+         7
+         8             102928 / 32763            3.14159265
+         8                                       3.14159265
+         8
+         9             103993 / 33102            3.141592653
+         9                                       3.141592653
+         9
+        10             521030 / 165849           3.1415926535
+        10                                       3.1415926535
+        10
+        11             833719 / 265381           3.14159265358
+        11                                       3.14159265358
+        11
+        12            4272943 / 1360120          3.141592653589
+        12                                       3.141592653589
+        12
+        13           20530996 / 6535219          3.1415926535897
+        13                                       3.1415926535897
+        13
+        14           63885804 / 20335483         3.14159265358979
+        14                                       3.14159265358979
+#>

34dd911d0fb0b10e73b55f735bf44171373e6b6e
mitmih <38176228+mitmih@users.noreply.github.com>
2020-07-01 00:34:09 +0300

    Update 02_SelectionSort.ps1

--- grokking_algorithms/02_SelectionSort.ps1
+++ grokking_algorithms/02_SelectionSort.ps1
@@ -1,3 +1,8 @@
 $Root = ($MyInvocation.MyCommand.Definition | Split-Path -Parent -Resolve)
 
 Import-Module -Force -Verbose ('{0}\grokking.psm1' -f $Root, ($Leaf -replace 'ps1', 'psm1'))
+
+Clear-Host
+
+
+SelectionSort ((0..9) + (0..-9))

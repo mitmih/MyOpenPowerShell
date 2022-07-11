5faac08ac10790c5104507ae45979853634b03e1
mitmih <38176228+mitmih@users.noreply.github.com>
2022-07-11 13:40:53 +0300

    Update decode.ps1

--- hacked_1st_Archie_puzzle_and_made_similar_one/decode.ps1
+++ hacked_1st_Archie_puzzle_and_made_similar_one/decode.ps1
@@ -1,12 +1,53 @@
-$MorseFile = Join-Path -Path "$env:USERPROFILE\Downloads" -ChildPath 'morseM.txt' -Resolve | Get-Item
+#Requires -Version 7.2  # скрипт разрабатывался и тестировался с интерпретатором "C:\Program Files\PowerShell\7\pwsh.exe"
 
-$Morse = ($MorseFile | Get-Content) -replace '−', '-' -replace '•', '.'
+<#
+.SYNOPSIS
+    decode txt file with morses to png image
+
+.DESCRIPTION
+    to decode your morse txt file to png image make this steps:
+        save decode.ps1 to your downloads
+        save https://pastebin.com/raw/zeFkDskS to 'zeFkDskS' file
+        run script (see examples)
+
+.EXAMPLE
+    pwsh -f "%USERPROFILE%\Downloads\decode.ps1" -m "%USERPROFILE%\Downloads\zeFkDskS"
+
+.EXAMPLE
+    pwsh -f "%USERPROFILE%\Downloads\decode.ps1" -m "%USERPROFILE%\Downloads\morseA.txt"
+
+.INPUTS
+    txt file with bytes encodes by Morse codes, where
+        ----- is 0
+        .---- is 1
+
+.OUTPUTS
+    png file
+
+.NOTES
+    (c) Mitry Mikhaylov
+
+.LINK
+    https://github.com/mitmih/MyOpenPowerShell
+
+#>
+
+
+[CmdletBinding()]
+param (
+    # path to file with morse codes
+    [Alias('m')]
+    [string] $MorseFile = (Join-Path -Path "$env:USERPROFILE\Downloads" -ChildPath 'morse.txt')
+)
+
+$ErrorActionPreference = "Stop"
+
+$MorseFile = Get-ChildItem -Path $MorseFile
+$Morse = (Get-Content -Path $MorseFile) -replace '−', '-' -replace '•', '.'
 
 if (($Morse | Measure-Object).Count -lt 2) { $Morse = $Morse -split '   ' }
 
 $MorseCodes = @{
-    '0'     = '-----'
-    '1'     = '.----'
     '-----' = '0'
     '.----' = '1'
 }
@@ -23,6 +64,17 @@ $Morse | ForEach-Object {
 }
 $lst[0..3],' ...', $lst[-3..-1] | Format-Table *
 
-$lst.byte | Set-Content -Force -AsByteStream -Path (Join-Path -Path "$env:USERPROFILE\Downloads" -ChildPath 'razgadka.png')
-
-$lst.char -join ''
+$pngFile = $MorseFile -replace 'txt', 'png'
+try
+{
+    # если в морзе было закодировано "data:image/png;base64,..."
+    [System.Convert]::FromBase64String(($lst.char -join '' -replace 'data:image/png;base64,', '')) | Set-Content -Force -AsByteStream -Path $pngFile
+}
+catch
+{
+    $lst.byte | Set-Content -Force -AsByteStream -Path $pngFile
+}
+finally
+{
+    Get-Item $pngFile | Format-Hex | Select-Object -First 5
+}
